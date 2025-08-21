15c11f23e9b69c99d96f95a5c2e238fd3ec8b857
Bilal-Ahmed35 <bilalahmedshah35@gmail.com>
2025-08-21 16:33:27 +0500

    adding css

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Bilal");
+console.log("BIlal " + (15 - 10));
+console.log(`BIlal ${15 - 10}`);
+
+let bilal = {
+  age: 22,
+  height: 5.7,
+  gender: "male",
+  status: "engaged",
+  alive: "true",
+};
+
+console.log(bilal);
